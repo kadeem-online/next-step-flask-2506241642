c78a58bc583c98bf0f640088688327c47069ade4
kadeem-online <kadeemalexander254@gmail.com>
2025-06-24 17:32:53 +0300

    test: added test for the test_client and the home page

--- tests/python/test_base.py
+++ tests/python/test_base.py
@@ -7,3 +7,25 @@ def test_app_initialization(app):
 
     assert app is not None
     assert app.config["TESTING"] is True
+
+
+def test_client_initialization(client):
+    """
+    Test that the client initializes correctly.
+    """
+
+    assert client is not None
+    assert hasattr(client, "get")
+    assert hasattr(client, "post")
+    assert hasattr(client, "put")
+    assert hasattr(client, "delete")
+
+
+def test_home_page(client):
+    """
+    Test that the homepage loads correctly
+    """
+
+    response = client.get('/')
+
+    assert response.status_code == 200
